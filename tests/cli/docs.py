09de1350ad6b887e0a202e57f1fbc1e30468cb0c
Christopher O'Brien <obriencj@gmail.com>
2020-10-08 08:51:59 -0400

    minor tweaks to doc tests

--- tests/cli/docs.py
+++ tests/cli/docs.py
@@ -17,7 +17,7 @@ from docutils.nodes import GenericNodeVisitor
 from docutils.parsers.rst import Parser
 from docutils.utils import new_document
 from mock import patch
-from nose.tools import assert_equal, assert_raises
+from nose.tools import assert_equal, assert_raises, assert_true
 from pkg_resources import EntryPoint
 from six import iteritems
 from six.moves import StringIO
@@ -25,24 +25,23 @@ from six.moves import StringIO
 from . import ENTRY_POINTS, GOptions
 
 
-def default_document(name):
+def load_rst(filename):
+    # use the docutils option parser to get defaults for our efforts
     opts = OptionParser(components=(Parser,))
     defaults = opts.get_default_values()
 
     # shut up the warnings
     defaults.warning_stream = StringIO()
 
-    return new_document(name, defaults)
-
-
-def load_rst(filename):
+    doc = new_document(filename, defaults)
     with open(filename) as f:
-        doc = default_document(filename)
         Parser().parse(f.read(), doc)
+
     return doc
 
 
 class UsageFinder(GenericNodeVisitor):
+
     def __init__(self, doc):
         GenericNodeVisitor.__init__(self, doc)
         self.found_usage = []
@@ -57,10 +56,19 @@ class UsageFinder(GenericNodeVisitor):
 
 
 def find_usage(filename):
+    """
+    Loads a reST doc from filename and finds literal blocks that start
+    with the words "usage: koji"
+
+    :rtype: str or None
+    """
+
     doc = load_rst(filename)
     finder = UsageFinder(doc)
     doc.walk(finder)
-    return finder.found_usage
+    found = finder.found_usage
+
+    return found[0] if found else None
 
 
 def check_command_help(cmdname):
@@ -78,25 +86,34 @@ def check_command_help(cmdname):
 
     command = cmd_cls(name)
 
+    # a fake goptions based on a copy of the koji defaults
     goptions = GOptions()
 
+    # launch the loaded command with the --help option, and collect
+    # its output. Also ensures that it causes the expected SystemExit
     with patch('sys.stdout', new=StringIO()) as out:
         with patch('sys.argv', new=["koji", name, "--help"]):
             assert_raises(SystemExit,
                           command, goptions, None, ("--help",))
         outhelp = out.getvalue()
 
+    # now we'll load the expected help output from the first literal
+    # block from the reST doc sources
     usage = find_usage("docs/commands/%s.rst" % name)
-    assert_equal(len(usage), 1)
+    assert_true(usage is not None)
 
-    expected = usage[0].strip()
+    # Then we can compare what we got from the command with what was
+    # in the docs
+    expected = usage.strip()
     found = outhelp.strip()
     assert_equal(expected, found)
 
 
 def test_command_help():
-    # verify the expected entry points resolve and can be
-    # initialized
+    # verify the expected entry points resolve and can be initialized,
+    # and that when invoked with --help they produce output that
+    # matches our documentation
+
     for name in ENTRY_POINTS:
         yield check_command_help, name
 
