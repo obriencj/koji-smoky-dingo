d6e826f01931b8534fd9a4c4073cf0d60cb7feac
Christopher O'Brien <obriencj@gmail.com>
2021-04-01 07:33:32 -0400

    comments to explain

--- tools/launch-build.sh
+++ tools/launch-build.sh
@@ -1,14 +1,22 @@
 #! /bin/bash
 
 
-. $(readlink -f $(dirname "$0"))/common.sh
+# Launches the build script to generate container images for the
+# various supported platforms. Platforms are indicated as suffixes to
+# the Containerfiles in this directory. Each platform will have an
+# image generated. As part of the image build process the KSD SRPM and
+# RPM will be created, and the RPM will be installed into the
+# image. Previous copies of the platform images will have their layers
+# reused if possible, but will be removed and pruned after the new
+# images are complete.
 
+
+. $(readlink -f $(dirname "$0"))/common.sh
 
 for PLATFORM in $(ksd_platforms "$@") ; do
     ksd_build_platform "$PLATFORM" || exit 1
     echo
 done
-
 
 ksd_prune
 
--- tools/rpmbuild.sh
+++ tools/rpmbuild.sh
@@ -3,7 +3,7 @@
 # WARNING!  This script is intended to be invoked from inside of a
 # container. It will attempt to install dependencies in order to build
 # an SRPM from a tarball, rebuild the SRPM into an RPM, and finally
-# install those built RPMs. See the various Containerfiles in this
+# install that built RPM. See the various Containerfiles in this
 # directory for reference.
 
 
