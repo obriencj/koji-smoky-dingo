9397eaab63a1eedb5a983eaac46d037da74eb65b
Christopher O'Brien <obriencj@gmail.com>
2021-08-27 14:50:04 -0400

    license

--- tools/common.sh
+++ tools/common.sh
@@ -1,3 +1,16 @@
+# This library is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this library; if not, see <http://www.gnu.org/licenses/>.
+
 
 # Collection of bash functions for use in building and testing this
 # project.
@@ -203,4 +216,5 @@ function ksd_platforms() {
 }
 
 
+#
 # The end.
