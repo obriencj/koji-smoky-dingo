147623c12121a27b4bbb85487193f75b27a3cb83
Christopher O'Brien <obriencj@gmail.com>
2024-04-09 13:04:35 -0400

    clean up and document

--- mypy/proxytype.py
+++ mypy/proxytype.py
@@ -1,23 +1,37 @@
-# This implementation is essentially a no-op. We'll be using it only
-# as a marker for the mypy_proxytype plugin itself.
+# This library is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this library; if not, see <http://www.gnu.org/licenses/>.
 
 
-from mypy.nodes import (
-    Argument, Decorator, FuncDef, OverloadedFuncDef, SymbolTableNode, Var,
-)
+"""
+proxytype - a mypy plugin and decorator
+
+The `proxytype.proxytype` class decorator is for use in cases where we
+need to provide static typing information for a class that dynamically
+proxies the methods of some other class. In our case that is
+specifically for the behavior of koji.MultiCallSession proxying to
+koji.ClientSession
+
+:author: Christopher O'Brien <obriencj@gmail.com>
+:license: GPL v3
+"""
+
+
+from mypy.nodes import Decorator, FuncDef, OverloadedFuncDef
 from mypy.plugin import ClassDefContext, MethodContext, Plugin
-
-from typing import (
-    Any, Callable, Generic, Optional, Type, TypeVar,
-    overload,
-)
+from typing import Generic, Type, TypeVar, overload
 
 
 __all__ = ( "proxytype", )
-
-
-def _identity(o):
-    return o
 
 
 PT = TypeVar("PT")
@@ -25,14 +39,54 @@ RT = TypeVar("RT")
 
 
 class ProxyTypeBuilder(Generic[PT, RT]):
+    # this class, and more specifically its __call__ method, are used
+    # as the sentinel to trigger the mypy plugin hook.
     def __call__(self, cls: type) -> type:
         return cls
+
+
+# This string needs to refer to the fully-qualified identifier for the
+# above class, which stands as our sentinel to trigger the plugin's
+# behavior.
+PTB_CALL = "proxytype.ProxyTypeBuilder.__call__"
 
 
 def proxytype(
         orig_class: Type[PT],
         return_wrapper: Type[RT]) -> ProxyTypeBuilder[PT, RT]:
-    return _identity  # type: ignore
+
+    """
+    class decorator which, via its ProxyTypeBuilder return type,
+    triggers augmentation of its wrapper class with the methods found
+    in the orig_class type, having their first (self) argument changed
+    to match the decorated class, and their return type to match the
+    return_wrapper's generic
+
+    The canonical example is MultiCallSession, eg.
+
+    ``
+    class ClientSession:
+        # lots of methods here
+        ...
+
+    class VirtualCall(Generic[T]):
+        result: T
+
+    @proxytype(ClientSession, VirtualCall)
+    class MultiCallSession:
+        # all methods from ClientSession magically copied into
+        # this during static analysys
+        ...
+    ``
+
+    for example then, a method on ClientSession such as
+      ``getPerms(self: ClientSession) -> List[str]``
+
+    will be recreated on MultiCallSession as
+      ``getPerms(self: MultiCallSession) -> VirtualCall[List[str]]``
+
+    """
+    return ProxyTypeBuilder()
 
 
 def clone_func(fn, cls, returntype):
@@ -50,7 +104,7 @@ def clone_func(fn, cls, returntype):
 
     cp = FuncDef(fn._name, None)
     cp.type = cpt
-    cp.info = cls
+    cp.info = cls  # this particular field took so long to debug
     # cp._fullname = fn.fullname
 
     return cp
@@ -74,6 +128,19 @@ def clone_overload(ov, cls, returntype):
 
 
 def decorate_proxytype(wrap, orig, virt):
+    """
+    Creates methods on wrap cloned from orig, modified to return
+    virt wrappers.
+
+    :param wrap: the type definition as decorated by
+    ``@proxytype(orig, virt)``
+
+    :param orig: the type definition to copy fields from
+
+    :param virt: the type definition for wrapping the original result
+    types in
+    """
+
     for name, sym in orig.type.names.items():
         if name.startswith("_"):
             continue
@@ -91,16 +158,7 @@ def decorate_proxytype(wrap, orig, virt):
     return None
 
 
-PTB_CALL = (f"{ProxyTypeBuilder.__module__}."
-            f"{ProxyTypeBuilder.__call__.__qualname__}")
-
-
 class ProxyTypePlugin(Plugin):
-
-    def __init__(self, options):
-        super().__init__(options)
-        self._proxytypes = {}
-
 
     def register_proxytype(self, ctx: MethodContext):
         args = ctx.type.args  # type: ignore
@@ -111,14 +169,6 @@ class ProxyTypePlugin(Plugin):
             virt = None
 
         wrap = ctx.context.info  # type: ignore
-        self._proxytypes[wrap.fullname] = (orig, virt)
-
-        # TODO: was thinking I could just associate the type fullname
-        # to its orig, virt vars and then use some other hook later to
-        # update the class definition, but that's been a damn
-        # struggle. So we're back to just decorating it immediately
-        # here.
-
         decorate_proxytype(wrap, orig, virt)
 
         return ctx.default_return_type
@@ -131,6 +181,8 @@ class ProxyTypePlugin(Plugin):
 
 
 def plugin(version: str):
+    # mypy plugin loading point
+
     return ProxyTypePlugin
 
 
